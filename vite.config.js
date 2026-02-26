55df06cb7fc9a3a916795cd28db841848989bf41
baday <bagus.hid@gmail.com>
2026-02-26 11:44:34 +0700

    add script

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: '/soul-journal.badaay.github.io/',
-})
+export default defineConfig(({ command }) => ({
+    base: command === 'serve' ? '/' : '/soul-journal.badaay.github.io/',
+}))
